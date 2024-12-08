a1d62b17ff465fcaceaa5a39bca69ba8c975905d
Abyan Dimas <abyandimas@Abyans-MacBook-Air.local>
2024-12-08 10:54:27 +0700

    update

---
+++ src/admin/app.ts
@@ -0,0 +1,32 @@
+export default {
+  config: {
+    translations: {
+      en: {
+        "Auth.form.welcome.title": "Selamat Datang!",
+        "Auth.form.welcome.subtitle": "Masuk ke Dashboard Admin",
+        "Auth.form.email.placeholder": "Ketikkan Nama Email Disini",
+        "Auth.form.error.code.provide": "Kode yang diberikan salah.",
+        "Auth.form.error.confirmed": "Email akun Anda belum dikonfirmasi.",
+        "Auth.form.active.label": "Aktif",
+        "Auth.form.button.forgot-password": "Kirim Email",
+        "Auth.form.button.go-home": "KEMBALI KE BERANDA",
+        "Auth.form.button.login": "Masuk",
+        "Auth.form.button.login.providers.error": "Kami tidak dapat menghubungkan Anda melalui penyedia yang dipilih.",
+        "Auth.form.button.login.strapi": "Masuk melalui Strapi",
+        "Auth.form.button.password-recovery": "Pemulihan Kata Sandi",
+        "Auth.form.button.register": "Mari mulai",
+        "Auth.form.confirmPassword.label": "Konfirmasi Kata Sandi",
+        "Auth.form.currentPassword.label": "Kata Sandi Saat Ini",
+        "Auth.form.email.label": "Email",
+        "Auth.components.Oops.text": "Akun Anda telah ditangguhkan.",
+        "Auth.components.Oops.text.admin": "Jika ini adalah kesalahan, silakan hubungi administrator Anda.",
+        "Auth.components.Oops.title": "Oops...",
+      },
+    },
+    locales: ["id"],
+    tutorials: false,
+    notifications: { releases: false },
+  },
+
+  bootstrap() {},
+};
---
+++ src/api/artikel/services/artikel.ts
@@ -0,0 +1,7 @@
+/**
+ * artikel service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::artikel.artikel');
